f4485aacef2d8397c5957262978b16065e3b8a28
Jacob Stovall <jacob.stovall@asu.edu>
2016-03-26 16:42:37 -0700

    Finished Creating Website

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var bodyParser = require("body-parser");
+var Datastore = require("nedb");
+var db = new Datastore({filename: "./emaildb.db"});
+
+db.loadDatabase();
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(express.static("public"));
+
+app.get('/', function(req,res){
+  res.sendFile(__dirname+"/index.html");
+});
+
+app.post("/",function(req,res){
+  var email = req.body.email;
+  console.log(email);
+  var doc = {email: email};
+  db.insert(doc);
+  res.end("Email Saved");
+});
+
+app.listen(3000,function(){
+  console.log("Example app listening on port 3000!");
+});
