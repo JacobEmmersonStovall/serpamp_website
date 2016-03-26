27c44b44bfee294b0def7861f441c092e00d165b
Jacob Stovall <jacob.stovall@asu.edu>
2016-03-26 16:53:35 -0700

    Attempted to add favicon

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var app = express();
 var bodyParser = require("body-parser");
+var favicon = require('serve-favicon');
 var Datastore = require("nedb");
 var db = new Datastore({filename: "./emaildb.db"});
 
 db.loadDatabase();
 
 app.use(bodyParser.urlencoded({extended:false}));
-
+app.use(favicon(__dirname+"/public/favicon.ico"))
 app.use(express.static("public"));
 
 app.get('/', function(req,res){
